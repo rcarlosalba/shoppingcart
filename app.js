e838795b13858c393b1f76f9b4a8a58a400a3b3d
Carlos Alba <hola@carlosalba.net>
2022-10-24 14:15:41 +0300

    Tiempo de salir, mañana lo hacemos de nuevo

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ eventListener();
 
 function eventListener() {
   listaCursos.addEventListener("click", agregarCurso);
+
+  carrito.addEventListener("click", eliminarCurso);
 }
 
 //funciones
@@ -31,9 +33,25 @@ function leerDatosCurso(curso) {
     id: curso.querySelector("a").getAttribute("data-id"),
     cantidad: 1,
   };
-  articulosCarrito = [...articulosCarrito, datosCursos];
 
-  carritoHTML(articulosCarrito);
+  const existe = articulosCarrito.some((curso) => curso.id === datosCursos.id);
+
+  if (existe) {
+    const todosLosCursos = articulosCarrito.map((curso) => {
+      if (curso.id === datosCursos.id) {
+        curso.cantidad += 1;
+        return curso;
+      } else {
+        return curso;
+      }
+    });
+    articulosCarrito = [...todosLosCursos];
+  } else {
+    articulosCarrito = [...articulosCarrito, datosCursos];
+
+    carritoHTML(articulosCarrito);
+    console.log(articulosCarrito);
+  }
 }
 
 function carritoHTML(articulos) {
@@ -47,6 +65,10 @@ function carritoHTML(articulos) {
         <td>${titulo}</td>
         <td>${precio}</td>
         <td>${cantidad}</td>
+        <td>
+            <a href="#" class="borrar-curso" data-id=${id}>X</a>
+        </td>
+
     `;
     contenedorCarrito.appendChild(row);
   });
@@ -56,4 +78,17 @@ function limpiarHTML() {
   while (contenedorCarrito.firstChild) {
     contenedorCarrito.removeChild(contenedorCarrito.firstChild);
   }
+}
+
+function eliminarCurso(e) {
+  if (e.target.classList.contains("borrar-curso")) {
+    const cursoSelectedId = e.target.getAttribute("data-id");
+
+    articulosCarrito = articulosCarrito.filter((curso) => {
+      curso.id !== cursoSelectedId;
+    });
+
+    carritoHTML();
+  }
+  console.log(articulosCarrito);
 }
